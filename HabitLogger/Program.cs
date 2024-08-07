4bc6078941a30d87f42b86c39cc3e9c6c7ef7270
Francisco Gaspar <nectryk@gmail.com>
2024-08-07 18:25:05 +0200

    feat: database and table creation

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HabitLoggerLibrary;
+
+SetupDatabase setupDatabase = new SetupDatabase();
+setupDatabase.InitializeDatabase();
---
+++ HabitLoggerLibrary/SetupDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Reflection.PortableExecutable;
+
+namespace HabitLoggerLibrary;
+
+public class SetupDatabase
+{
+    private const string ConnectionString = "Data Source=habit.db";
+    
+    public SetupDatabase()
+    {
+        
+    }
+
+    public void InitializeDatabase()
+    {
+        using (SqliteConnection connection = new SqliteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS ""DrinkWater"" (
+	                            ""Id""	INTEGER NOT NULL,
+	                            ""Day""	INTEGER NOT NULL,
+	                            ""Quantity""	INTEGER NOT NULL,
+	                            PRIMARY KEY(""Id"" AUTOINCREMENT)
+                                );";
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
